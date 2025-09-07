3e90c274844b78d68575192e53fdbfee6623736d
heojeongbo <heojungbo@gmail.com>
2025-09-07 16:55:04 +0900

    style: fix formatting issues

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,20 @@
-import * as vscode from 'vscode';
-import { FastlaneCompletionProvider } from './completionProvider';
+import * as vscode from "vscode";
+import { FastlaneCompletionProvider } from "./completionProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-    const provider = new FastlaneCompletionProvider();
-    
-    const disposable = vscode.languages.registerCompletionItemProvider(
-        { scheme: 'file', language: 'fastfile' },
-        provider,
-        ':', '(', '"', "'", ' '
-    );
+	const provider = new FastlaneCompletionProvider();
 
-    context.subscriptions.push(disposable);
+	const disposable = vscode.languages.registerCompletionItemProvider(
+		{ scheme: "file", language: "fastfile" },
+		provider,
+		":",
+		"(",
+		'"',
+		"'",
+		" "
+	);
+
+	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
